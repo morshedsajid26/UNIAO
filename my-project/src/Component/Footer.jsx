3ed0532019d80d0cdcaf38b8ab1a9624c9de5caa
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-01-13 00:28:17 +0600

    footer running

--- my-project/src/Component/Footer.jsx
+++ my-project/src/Component/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Container from '../Layer/Conatiner'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className=' max-w-[1862px] mx-auto  bg-gradient-to-l from-[#18C5E1] from-0% via-[#8666EF] via-30% to-[#FB01FF] to-100% bg-clip-border border-transparent  '>
+        <Container className=' px-[100px]'>
+
+            <h1 className='text-white font-OS font-bold text-6xl'>footer</h1>
+
+        </Container>
+    </div>
   )
 }
 
