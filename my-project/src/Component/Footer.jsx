3cbd5edf8945df2c5bfcb313ca501d60c5ae2468
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-01-13 23:59:40 +0600

    footer running

--- my-project/src/Component/Footer.jsx
+++ my-project/src/Component/Footer.jsx
@@ -3,10 +3,20 @@ import Container from '../Layer/Conatiner'
 
 const Footer = () => {
   return (
-    <div className=' max-w-[1862px] mx-auto  bg-gradient-to-l from-[#18C5E1] from-0% via-[#8666EF] via-30% to-[#FB01FF] to-100% bg-clip-border border-transparent  '>
+    <div className=' max-w-[1862px] mx-auto  bg-[#1E2127] g-gradient-to-l from-[#18C5E1] from-0% via-[#8666EF] via-30% to-[#FB01FF] to-100% bg-clip-border border-transparent  '>
         <Container className=' px-[100px]'>
 
-            <h1 className='text-white font-OS font-bold text-6xl'>footer</h1>
+           <div className="top grid grid-cols-12 justify-between gap-10">
+            <div className="child col-span-3 bg-slate-500">
+              <h3 className='text-[28px] text-[#FFFFFF] font-medium font- '>Trustful</h3>
+            </div>
+
+
+            <div className="child col-span-2 bg-slate-500">1</div>
+            <div className="child col-span-2 bg-slate-500">1</div>
+            <div className="child col-span-3 bg-slate-500">1</div>
+            <div className="child col-span-2 bg-slate-500">1</div>
+           </div>
 
         </Container>
     </div>
