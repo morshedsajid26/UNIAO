7d0c6cc6949032d39748266167699f20141a3f28
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-01-17 00:57:33 +0600

    footer running

--- my-project/src/Component/Footer.jsx
+++ my-project/src/Component/Footer.jsx
@@ -3,11 +3,14 @@ import Container from '../Layer/Conatiner'
 
 import star from '/star.png'
 import trustpilot from '/trustpilot.png'
+import { FaFacebookF, FaInstagram, FaLinkedinIn, FaXTwitter } from 'react-icons/fa6'
+import sponsor from '/sponsor.png'
 
+import FooterLogo from '/FooterLogo.png'
 
 const Footer = () => {
   return (
-    <div className=' max-w-[1862px] mx-auto  bg-[#1E2127] g-gradient-to-l from-[#18C5E1] from-0% via-[#8666EF] via-30% to-[#FB01FF] to-100% bg-clip-border border-transparent  '>
+    <div className=' max-w-[1862px] mx-auto  bg-[#1E2127] g-gradient-to-l from-[#18C5E1] from-0% via-[#8666EF] via-30% to-[#FB01FF] to-100% bg-clip-border border-transparent pt-[148px] pb-[26px]  '>
         <Container className=' px-[100px]'>
 
            <div className="top grid grid-cols-12 justify-between gap-10">
@@ -20,40 +23,72 @@ const Footer = () => {
               </div>
 
               <div className="rating"></div>
+
+
+              <p className='text-[#E414FB] font-Inter text-[25px]'>100% Money Back</p>
             </div>
 
 
-            <div className="child col-span-2 bg-slate-500">
-              <ul>
+            <div className="child col-span-2">
+             
                 <p className='text-white font-pop text-[24px] font-medium'>Company</p>
 
+                <ul className=' flex flex-col gap-[50px] mt-[30px]'>
                 <li className='text-white/60 font-Inter text-[24px] '>Home</li>
                 <li className='text-white/60 font-Inter text-[24px] '>Our Story</li>
                 <li className='text-white/60 font-Inter text-[24px] '>Blog</li>
                 <li className='text-white/60 font-Inter text-[24px] '>Contact Us</li>
               </ul>
             </div>
-            <div className="child col-span-2 bg-slate-500">
-            <ul>
+            <div className="child col-span-2">
+            
                 <p className='text-white font-pop text-[24px] font-medium'>Orders</p>
-
+                
+                <ul className=' flex flex-col gap-[50px] mt-[30px]'>
                 <li className='text-white/60 font-Inter text-[24px] '>My Orders</li>
                 <li className='text-white/60 font-Inter text-[24px] '>Refund Policy</li>
                 <li className='text-white/60 font-Inter text-[24px] '>Cancelations</li>
                 <li className='text-white/60 font-Inter text-[24px] '>Help Center</li>
               </ul>
             </div>
-            <div className="child col-span-3 bg-slate-500">
-            <ul>
+            <div className="child col-span-3">
+            
                 <p className='text-white font-pop text-[24px] font-medium'>Resources</p>
-
+                <ul className=' flex flex-col gap-[50px] mt-[30px]'>
                 <li className='text-white/60 font-Inter text-[24px] '>Why do Customers Love Us</li>
                 <li className='text-white/60 font-Inter text-[24px] '>Redeem Argentina and Turkey</li>
                 <li className='text-white/60 font-Inter text-[24px] '>How to Sell?</li>
                 <li className='text-white/60 font-Inter text-[24px] '>How Become affliate?</li>
               </ul>
             </div>
-            <div className="child col-span-2 bg-slate-500">1</div>
+            <div className="child col-span-2">
+            <p className='text-white font-pop text-[24px] font-medium'>Follow Us</p>
+
+            <ul className='flex  gap-2'>
+              <li className='w-[49px] h-[49px] rounded-full bg-gradient-to-l from-[#00DBDE] from-1% to-[#FC00FF] to-100% flex items-center justify-center  '><FaXTwitter /></li>
+              <li className='w-[49px] h-[49px] rounded-full bg-gradient-to-l from-[#00DBDE] from-1% to-[#FC00FF] to-100% flex items-center justify-center  '><FaInstagram /></li>
+              <li className='w-[49px] h-[49px] rounded-full bg-gradient-to-l from-[#00DBDE] from-1% to-[#FC00FF] to-100% flex items-center justify-center  '><FaFacebookF /></li>
+              <li className='w-[49px] h-[49px] rounded-full bg-gradient-to-l from-[#00DBDE] from-1% to-[#FC00FF] to-100% flex items-center justify-center  '><FaLinkedinIn /></li>
+            </ul>
+            </div>
+           </div>
+
+           <div className="middle flex justify-center mt-[144px] mb-[119px]">
+            <img src={sponsor} alt="" />
+           </div>
+
+           <div className='bottom flex justify-between items-center'>
+
+            <p className='text-[22px] text-white font-Inter'>Vbrae © Copyright 2023</p>
+
+            <img src={FooterLogo} alt="" />
+
+            <p className='flex gap-2 text-[22px] text-white font-Inter'>
+            Privacy Policy
+              <span>|</span>
+              Terms of Service
+            </p>
+
            </div>
 
         </Container>
