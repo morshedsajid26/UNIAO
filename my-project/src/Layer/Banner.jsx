fe57da708079aa25adb3d15be77af071d9697a76
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-01-17 17:28:26 +0600

    banner done

--- my-project/src/Layer/Banner.jsx
+++ my-project/src/Layer/Banner.jsx
@@ -13,14 +13,31 @@ import game4 from '/game4.png'
 import game5 from '/game5.png'
 
 
+import path1 from '/path-1.png'
+import path2 from '/path-2.png'
+import path3 from '/path-3.png'
+import path4 from '/path-4.png'
+import line from '/line.png'
+
+
 const Banner = () => {
   return (
     <div>
 
-<Container className='bg-[url("/public/banner.png")] h-[730px] after:rounded-[25px]  bg-no-repeat pb-[87px] pt-[103px] relative after:absolute after:content-[""] after:h-full  after:w-full  after:bg-black/10 after:top-0 after:left-0 '>
+<Container className='bg-[url("/public/banner.png")] h-[730px] after:rounded-[25px]  bg-no-repeat pb-[87px] pt-[103px] relative after:absolute after:content-[""] after:h-full  after:w-full  after:bg-black/10 after:top-0 after:left-0 after:-z-30 '>
 
 <div className='pl-[69px] z-30'>
-<img src={bannerIcons} alt="" />
+
+
+<div className='icons flex items-center gap-7'>
+
+    <img src={path1} alt="" />
+    <img src={path2} alt="" />
+    <img className='w-[1px] h-[29px]' src={line} alt="" />
+    <img src={path3} alt="" />
+    <img src={path4} alt="" />
+
+</div>
 
 <h3 className='font-Inter text-white text-[68px] mt-[17px]'>Death Stranding</h3>
 
@@ -48,11 +65,11 @@ const Banner = () => {
 
 
 <div className='flex gap-[35px] items-end mt-[103px]'>
- <img src={game1} alt="" />
- <img src={game2} alt="" />
- <img src={game3} alt="" />
- <img src={game4} alt="" />
- <img src={game5} alt="" />
+ <img className='border-4 border-white/30 rounded-[25px] p-1' src={game1} alt="" />
+ <img className='border-4 border-white/30 rounded-[25px] p-1' src={game2} alt="" />
+ <img className='border-4 border-white/30 rounded-[25px] p-1' src={game3} alt="" />
+ <img className='border-4 border-white/30 rounded-[25px] p-1' src={game4} alt="" />
+ <img className='border-4 border-white/30 rounded-[25px] p-1' src={game5} alt="" />
 </div>
 
 </div>
