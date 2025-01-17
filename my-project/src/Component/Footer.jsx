00b39712f6409d31d0593803efa2f01c42e4cdcb
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-01-17 15:35:01 +0600

    footer border running

--- my-project/src/Component/Footer.jsx
+++ my-project/src/Component/Footer.jsx
@@ -10,88 +10,90 @@ import FooterLogo from '/FooterLogo.png'
 
 const Footer = () => {
   return (
-    <div className=' max-w-[1862px] mx-auto  bg-[#1E2127] g-gradient-to-l from-[#18C5E1] from-0% via-[#8666EF] via-30% to-[#FB01FF] to-100% bg-clip-border border-transparent pt-[148px] pb-[26px]  '>
-        <Container className=' px-[100px]'>
+    <div className=' max-w-[1862px] mx-auto   bg-gradient-to-r from-[#18C5E1] from-0% via-[#8666EF] via-30% to-[#FB01FF] to-100% p-[2px] rounded-[25px]  '>
+       <div className='bg-[#1E2127] rounded-[25px]'>
+       <Container className=' pt-[148px] pb-6'>
 
-           <div className="top grid grid-cols-12 justify-between gap-10">
-            <div className="child col-span-3 bg-slate-500">
-              <h3 className='text-[28px] text-[#FFFFFF] font-medium font-pop '>Trustful</h3>
+<div className="top grid grid-cols-12 justify-between gap-10">
+ <div className="child col-span-3 bg-slate-500">
+   <h3 className='text-[28px] text-[#FFFFFF] font-medium font-pop '>Trustful</h3>
 
-              <div className="logo flex items-end gap-[6px] ">
-                <img className='w-[61.24px] h-[58.73px]' src={star} alt="" />
-                <img className='w-[188.74px] h-[41.16px]' src={trustpilot} alt="" />
-              </div>
+   <div className="logo flex items-end gap-[6px] ">
+     <img className='w-[61.24px] h-[58.73px]' src={star} alt="" />
+     <img className='w-[188.74px] h-[41.16px]' src={trustpilot} alt="" />
+   </div>
 
-              <div className="rating"></div>
-
-
-              <p className='text-[#E414FB] font-Inter text-[25px]'>100% Money Back</p>
-            </div>
+   <div className="rating"></div>
 
 
-            <div className="child col-span-2">
-             
-                <p className='text-white font-pop text-[24px] font-medium'>Company</p>
+   <p className='text-[#E414FB] font-Inter text-[25px]'>100% Money Back</p>
+ </div>
 
-                <ul className=' flex flex-col gap-[50px] mt-[30px]'>
-                <li className='text-white/60 font-Inter text-[24px] '>Home</li>
-                <li className='text-white/60 font-Inter text-[24px] '>Our Story</li>
-                <li className='text-white/60 font-Inter text-[24px] '>Blog</li>
-                <li className='text-white/60 font-Inter text-[24px] '>Contact Us</li>
-              </ul>
-            </div>
-            <div className="child col-span-2">
-            
-                <p className='text-white font-pop text-[24px] font-medium'>Orders</p>
-                
-                <ul className=' flex flex-col gap-[50px] mt-[30px]'>
-                <li className='text-white/60 font-Inter text-[24px] '>My Orders</li>
-                <li className='text-white/60 font-Inter text-[24px] '>Refund Policy</li>
-                <li className='text-white/60 font-Inter text-[24px] '>Cancelations</li>
-                <li className='text-white/60 font-Inter text-[24px] '>Help Center</li>
-              </ul>
-            </div>
-            <div className="child col-span-3">
-            
-                <p className='text-white font-pop text-[24px] font-medium'>Resources</p>
-                <ul className=' flex flex-col gap-[50px] mt-[30px]'>
-                <li className='text-white/60 font-Inter text-[24px] '>Why do Customers Love Us</li>
-                <li className='text-white/60 font-Inter text-[24px] '>Redeem Argentina and Turkey</li>
-                <li className='text-white/60 font-Inter text-[24px] '>How to Sell?</li>
-                <li className='text-white/60 font-Inter text-[24px] '>How Become affliate?</li>
-              </ul>
-            </div>
-            <div className="child col-span-2">
-            <p className='text-white font-pop text-[24px] font-medium'>Follow Us</p>
 
-            <ul className='flex  gap-2'>
-              <li className='w-[49px] h-[49px] rounded-full bg-gradient-to-l from-[#00DBDE] from-1% to-[#FC00FF] to-100% flex items-center justify-center  '><FaXTwitter /></li>
-              <li className='w-[49px] h-[49px] rounded-full bg-gradient-to-l from-[#00DBDE] from-1% to-[#FC00FF] to-100% flex items-center justify-center  '><FaInstagram /></li>
-              <li className='w-[49px] h-[49px] rounded-full bg-gradient-to-l from-[#00DBDE] from-1% to-[#FC00FF] to-100% flex items-center justify-center  '><FaFacebookF /></li>
-              <li className='w-[49px] h-[49px] rounded-full bg-gradient-to-l from-[#00DBDE] from-1% to-[#FC00FF] to-100% flex items-center justify-center  '><FaLinkedinIn /></li>
-            </ul>
-            </div>
-           </div>
+ <div className="child col-span-2">
+  
+     <p className='text-white font-pop text-[24px] font-medium'>Company</p>
 
-           <div className="middle flex justify-center mt-[144px] mb-[119px]">
-            <img src={sponsor} alt="" />
-           </div>
+     <ul className=' flex flex-col gap-[50px] mt-[30px]'>
+     <li className='text-white/60 font-Inter text-[24px] '>Home</li>
+     <li className='text-white/60 font-Inter text-[24px] '>Our Story</li>
+     <li className='text-white/60 font-Inter text-[24px] '>Blog</li>
+     <li className='text-white/60 font-Inter text-[24px] '>Contact Us</li>
+   </ul>
+ </div>
+ <div className="child col-span-2">
+ 
+     <p className='text-white font-pop text-[24px] font-medium'>Orders</p>
+     
+     <ul className=' flex flex-col gap-[50px] mt-[30px]'>
+     <li className='text-white/60 font-Inter text-[24px] '>My Orders</li>
+     <li className='text-white/60 font-Inter text-[24px] '>Refund Policy</li>
+     <li className='text-white/60 font-Inter text-[24px] '>Cancelations</li>
+     <li className='text-white/60 font-Inter text-[24px] '>Help Center</li>
+   </ul>
+ </div>
+ <div className="child col-span-3">
+ 
+     <p className='text-white font-pop text-[24px] font-medium'>Resources</p>
+     <ul className=' flex flex-col gap-[50px] mt-[30px]'>
+     <li className='text-white/60 font-Inter text-[24px] '>Why do Customers Love Us</li>
+     <li className='text-white/60 font-Inter text-[24px] '>Redeem Argentina and Turkey</li>
+     <li className='text-white/60 font-Inter text-[24px] '>How to Sell?</li>
+     <li className='text-white/60 font-Inter text-[24px] '>How Become affliate?</li>
+   </ul>
+ </div>
+ <div className="child col-span-2">
+ <p className='text-white font-pop text-[24px] font-medium'>Follow Us</p>
 
-           <div className='bottom flex justify-between items-center'>
+ <ul className='flex  gap-2'>
+   <li className='w-[49px] h-[49px] rounded-full bg-gradient-to-l from-[#00DBDE] from-1% to-[#FC00FF] to-100% flex items-center justify-center  '><FaXTwitter /></li>
+   <li className='w-[49px] h-[49px] rounded-full bg-gradient-to-l from-[#00DBDE] from-1% to-[#FC00FF] to-100% flex items-center justify-center  '><FaInstagram /></li>
+   <li className='w-[49px] h-[49px] rounded-full bg-gradient-to-l from-[#00DBDE] from-1% to-[#FC00FF] to-100% flex items-center justify-center  '><FaFacebookF /></li>
+   <li className='w-[49px] h-[49px] rounded-full bg-gradient-to-l from-[#00DBDE] from-1% to-[#FC00FF] to-100% flex items-center justify-center  '><FaLinkedinIn /></li>
+ </ul>
+ </div>
+</div>
 
-            <p className='text-[22px] text-white font-Inter'>Vbrae © Copyright 2023</p>
+<div className="middle flex justify-center mt-[144px] mb-[119px]">
+ <img src={sponsor} alt="" />
+</div>
 
-            <img src={FooterLogo} alt="" />
+<div className='bottom flex justify-between items-center'>
 
-            <p className='flex gap-2 text-[22px] text-white font-Inter'>
-            Privacy Policy
-              <span>|</span>
-              Terms of Service
-            </p>
+ <p className='text-[22px] text-white font-Inter'>Vbrae © Copyright 2023</p>
 
-           </div>
+ <img src={FooterLogo} alt="" />
 
-        </Container>
+ <p className='flex gap-2 text-[22px] text-white font-Inter'>
+ Privacy Policy
+   <span>|</span>
+   Terms of Service
+ </p>
+
+</div>
+
+</Container>
+       </div>
     </div>
   )
 }
---
+++ my-project/src/Layer/Banner.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+import Container from '../Layer/Conatiner'
+
+import bannerIcons from '/bannerIcons.png'
+import { MdAddShoppingCart } from 'react-icons/md'
+import { IoStarSharp } from 'react-icons/io5'
+
+import game1 from '/game1.png'
+import game2 from '/game2.png'
+import game3 from '/game3.png'
+import game4 from '/game4.png'
+import game5 from '/game5.png'
+
+
+const Banner = () => {
+  return (
+    <div>
+
+<Container className='bg-[url("/public/banner.png")] h-[730px] after:rounded-[25px]  bg-no-repeat pb-[87px] pt-[103px] relative after:absolute after:content-[""] after:h-full  after:w-full  after:bg-black/10 after:top-0 after:left-0 '>
+
+<div className='pl-[69px] z-30'>
+<img src={bannerIcons} alt="" />
+
+<h3 className='font-Inter text-white text-[68px] mt-[17px]'>Death Stranding</h3>
+
+<div className='flex gap-5 items-start  mt-4'>
+ <div >
+   <p className='font-OS text-white/50 text-[18px]  '>From 
+   <del className='text-[28px] ml-[9px] mr-4'>$70</del>
+    to 
+    <span className='text-[28px] text-[#E614FC] ml-[6px]'>$59,99 </span></p>
+   <button className='flex  items-center gap-2 p-4 bg-gradient-to-r from-[#00DBDE] from-1% to-[#FC00FF] to-100% rounded-xl mt-[26px] cursor-pointer'>
+   <MdAddShoppingCart className='w-[33px] h-[29px] text-white ' />
+   <p className='font-OS text-[24px] text-white'>Buy Now</p>
+   </button>
+ </div>
+
+ <div className='flex gap-[3px]'>
+ <IoStarSharp className='w-[30px] h-[28.5px] bg-gradient-to-r from-[#00DBDE] from-1% to-[#FC00FF] to-100% bg-clip-text text-white' />
+ <IoStarSharp className='w-[30px] h-[28.5px] bg-gradient-to-r from-[#00DBDE] from-1% to-[#FC00FF] to-100% bg-clip-text text-white' />
+ <IoStarSharp className='w-[30px] h-[28.5px] bg-gradient-to-r from-[#00DBDE] from-1% to-[#FC00FF] to-100% bg-clip-text text-white' />
+ <IoStarSharp className='w-[30px] h-[28.5px] bg-gradient-to-r from-[#00DBDE] from-1% to-[#FC00FF] to-100% bg-clip-text text-white' />
+ <IoStarSharp className='w-[30px] h-[28.5px] bg-gradient-to-r from-[#00DBDE] from-1% to-[#FC00FF] to-100% bg-clip-text text-white' />
+ </div>
+
+</div>
+
+
+<div className='flex gap-[35px] items-end mt-[103px]'>
+ <img src={game1} alt="" />
+ <img src={game2} alt="" />
+ <img src={game3} alt="" />
+ <img src={game4} alt="" />
+ <img src={game5} alt="" />
+</div>
+
+</div>
+
+
+ </Container>
+    </div>
+  )
+}
+
+export default Banner
