9210ebe023a7305997839f162e51e98607ceebe5
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-01-25 23:54:12 +0600

    gift card responsive

--- my-project/src/Component/GiftCard.jsx
+++ my-project/src/Component/GiftCard.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const GiftCard = ({imageClass,image,heading,price,children}) => {
   return (
-    <div className='w-[327px] md:w-[524px] h-[148px] bg-[#1E2127] rounded-[20px] flex items-center  px-2.5 '>
+    <div className='w-[327px] md:w-[524px] h-[88px] md:h-[148px] bg-[#1E2127] rounded-[20px] flex items-center  px-2.5 '>
 
-        <div className={`w-[128px] h-[128px]  rounded-[20px] flex items-center justify-center ${imageClass}`}>
+        <div className={`w-[72px] md:w-[128px] h-[72px]  md:h-[128px]  rounded-[20px] flex items-center justify-center ${imageClass}`}>
             <img  src={image} alt="" />
         </div>
 
         <div className="text ml-[26px]">
-            <h3 className='text-[28px] text-white font-OS'>{heading}</h3>
+            <h3 className='text-[16px] md:text-[28px] text-white font-OS'>{heading}</h3>
 
-            
-                <p className='font-OS text-[28px] text-[#E614FC]'>{price}</p>
+           
+                <p className='font-OS text-[16px] md:text-[28px] text-[#E614FC]'>{price}</p>
             
         </div>
 
