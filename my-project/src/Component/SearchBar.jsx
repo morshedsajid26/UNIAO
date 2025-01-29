ec13b18052032006e0427cb8710d889a03bd9b4d
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-01-29 15:06:13 +0600

    searchbar responsive

--- my-project/src/Component/SearchBar.jsx
+++ my-project/src/Component/SearchBar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import logo from '/logo.png'
 
+import menu from '/MenuBar.png'
+
 import { IoSearch } from "react-icons/io5";
 import { AiFillDollarCircle } from "react-icons/ai";
 import { FaHeart } from "react-icons/fa";
@@ -15,8 +17,9 @@ const SearchBar = () => {
   return (
     <div>
         <Container className='border-b border-white/10 pt-[50px] pb-[36px] flex items-center justify-between'>
-            <div className="logo w-[213.63px]">
-                <img src={logo} alt="logo.png" />
+            <div className="logo md:w-[213.63px] flex gap-[14px] items-center">
+              <img className='block md:hidden' src={menu} alt="" />
+                <img className='w-[121.16px] md:w-full' src={logo} alt="logo.png" />
             </div>
 
             <div className="searchBar w-[853.45px] relative md:block hidden">
@@ -26,20 +29,22 @@ const SearchBar = () => {
                 
             </div>
 
-            <div className="icon w-[246.11px] flex justify-between items-center ">
-            <AiFillDollarCircle className='w-[37.11px] h-[37.11px] text-white' />
-            <FaHeart className='w-[36.27px] h-[32.3px] text-white'/>
+            <div className="icon  md:w-[246.11px] flex gap-[14px] md:gap-[34px] items-center ">
+            <AiFillDollarCircle className='w-[24.3px] md:w-[37.11px] h-[24.3px] md:h-[37.11px] text-white' />
+            <FaHeart className='w-[23.98px] md:w-[36.27px] h-[23.36px] md:h-[32.3px] text-white'/>
 
 
             <div className='relative'>
-            <FaShoppingBag className='w-[33.56px] h-[38.95px] text-white  ' /> 
+            <FaShoppingBag className='w-[22.19px] md:w-[33.56px] h-[25.76px] md:h-[38.95px] text-white  ' /> 
 
             
-            <div className='w-7 h-7 rounded-full bg-gradient-to-r from-[#00DBDE] from-1% to-[#FC00FF] to-100% flex items-center justify-center text-white font-OS font-bold text-[19px] absolute -top-[9px] -right-[14px] '>2
+            {/* <div className='w-[17px] md:w-7 h-[17px] md:h-7 rounded-full bg-gradient-to-r from-[#00DBDE] from-1% to-[#FC00FF] to-100% flex items-center justify-center text-white font-OS font-bold text-[11px] md:text-[19px] absolute -top-[9px] -right-[14px] '>2
 
-            </div>
+            </div> */}
+
+
             </div>          
-            <FaCircleUser className='w-[37.06px] h-[37.06px] text-white' />
+            <FaCircleUser className='w-[24.51px] md:w-[37.06px] h-[24.5px] md:h-[37.06px] text-white' />
 
 
             </div>
