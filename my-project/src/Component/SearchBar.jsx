f8c9125f30ef4cd563f7933a617714a468534b4c
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-01-29 15:08:02 +0600

    updated

--- my-project/src/Component/SearchBar.jsx
+++ my-project/src/Component/SearchBar.jsx
@@ -16,7 +16,7 @@ import Container from '../Layer/Conatiner';
 const SearchBar = () => {
   return (
     <div>
-        <Container className='border-b border-white/10 pt-[50px] pb-[36px] flex items-center justify-between'>
+        <Container className='border-b border-white/10 pt-[50px] pb-[18.5px] md:pb-[36px] flex items-center justify-between'>
             <div className="logo md:w-[213.63px] flex gap-[14px] items-center">
               <img className='block md:hidden' src={menu} alt="" />
                 <img className='w-[121.16px] md:w-full' src={logo} alt="logo.png" />
