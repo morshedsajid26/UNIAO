64eccd1746dacc386e030c9980037dedcd635e75
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-01-29 14:30:15 +0600

    container updated

--- my-project/src/Layer/Conatiner.jsx
+++ my-project/src/Layer/Conatiner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({children,className}) => {
   return (
-    <div className={` max-w-[1664px] mx-auto ${className}`}>{children}</div>
+    <div className={` max-w-[327px] md:max-w-[1664px] mx-auto ${className}`}>{children}</div>
   )
 }
 
