5afa1aab8b06f7eb1e06447e270fdc8810f6bdb9
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-01-29 14:53:34 +0600

    new arrivals responsive

--- my-project/src/Component/GameCard.jsx
+++ my-project/src/Component/GameCard.jsx
@@ -11,17 +11,17 @@ import { IoStarSharp } from 'react-icons/io5'
 
 const GameCard = ({src}) => {
   return (
-    <div className='w-[157px] md:w-[393px] bg-gradient-to-t from-[#18C5E1] from-0% via-[#8666EF] via-47% to-[#FB01FF] to-100% p-[2px] rounded-[25px]'>
-        <div className='bg-[#15181D] px-3 py-[14px] rounded-[25px]'>
+    <div className='w-[157px] md:w-[393px] bg-gradient-to-t from-[#18C5E1] from-0% via-[#8666EF] via-47% to-[#FB01FF] to-100% p-[2px] rounded-[20px] md:rounded-[25px]'>
+        <div className='bg-[#15181D] p-[6px] md:px-3 md:py-[14px] rounded-[20px] md:rounded-[25px]'>
             <div className="img ">
                 <img className='w-[146px] h-[114px] md:w-full md:h-full' src={src} alt="" />
             </div>
 
             
-                <p className='font-OS  text-[14px] md:text-[28px] text-white mt-[17px]'>Sekiro Shadows Die...</p>
+                <p className='font-OS  text-[14px] md:text-[28px] text-white mt-[5px] md:mt-[17px]'>Sekiro Shadows Die...</p>
             
-            <div className='flex gap-[31px] items-center mt-4 mb-[35px]'>
-                <div className='flex gap-2.5'>
+            <div className='flex gap-[3px] md:gap-[31px] md:items-center mt-[5px] md:mt-4 mb-[8px] md:mb-[35px]'>
+                <div className='flex gap-[2px] md:gap-2.5'>
                 <p className='flex flex-col font-OS text-[#78797D] text-[10px] md:text-[18px]'>From 
                     <del>$70 to</del>
                 </p>
@@ -29,7 +29,7 @@ const GameCard = ({src}) => {
                 <p className='text-[14px] md:text-[28px] text-[#E614FC] font-OS'>$59,99</p>
                 </div>
 
-                <div className="starIcon flex gap-[3px]">
+                <div className="starIcon flex gap-[1px] md:gap-[3px]">
                     <IoStarSharp className='w-[10.5px] md:w-[29px] h-[10.1px] md:h-7 text-[#16C7E1]'/>
                     <IoStarSharp className='w-[10.5px] md:w-[29px] h-[10.1px] md:h-7 text-[#16C7E1]'/>
                     <IoStarSharp className='w-[10.5px] md:w-[29px] h-[10.1px] md:h-7 text-[#16C7E1]'/>
@@ -38,7 +38,7 @@ const GameCard = ({src}) => {
                 </div>
             </div>
 
-            <div className="icon flex gap-7 justify-center">
+            <div className="icon flex gap-[11px] md:gap-7 justify-center">
             <img className='md:w-[39px] w-[15.61px] h-[12.05px] md:h-[30px]' src={icon1} alt="" />
             <img className='md:w-[30px] w-3 h-3 md:h-[30px]' src={icon2} alt="" />
             <img className='md:h-[30px] md:w-[30px] h-3 w-3' src={icon3} alt="" />
--- my-project/src/Layer/NewArrivals.jsx
+++ my-project/src/Layer/NewArrivals.jsx
@@ -13,16 +13,20 @@ const NewArrivals = () => {
     <div>
         <Container>
             <div className='flex items-center justify-center gap-[43px] mb-[35px]'>
-                <h1 className='text-[36px] font-OS text-white'>New Arrivals</h1>
-                <div className='h-[2px] w-[1140px] bg-custom-gradient'
+                <h1 className='text-[16px] md:text-[36px] font-OS text-white'>New Arrivals</h1>
+                <div className='h-[1px] md:h-[2px] w-[66px] md:w-[1140px] bg-custom-gradient'
                 ></div>
 
-                <button className='font-OS text-white text-[24px] py-4 px-[18px] border rounded-[10px] cursor-pointer '>
-                    Browse All
-                </button>
+
+
+                
+
+                <button className='border py-[7px] md:py-3 px-[10.5px] md:px-[18px] text-white font-OS text-[12px] md:text-[24px] rounded-[10px]'>
+                        Browse All
+                    </button>
             </div>
 
-            <div className=' grid grid-cols-4 grid-rows-2 gap-[47px]'>
+            <div className=' grid grid-cols-2 md:grid-cols-4 md:grid-rows-2 gap-[47px]'>
                 <GameCard
                 src={card1}/>
 
