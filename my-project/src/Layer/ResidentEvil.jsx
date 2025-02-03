2145f238226ae2ab24257a57b795c2069f20afef
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-02-03 22:28:56 +0600

    p adding

--- my-project/src/Layer/ResidentEvil.jsx
+++ my-project/src/Layer/ResidentEvil.jsx
@@ -12,7 +12,7 @@ import resident from '/resident.png'
 
 const ResidentEvil = () => {
   return (
-    <div className='py-10'>
+    <div className='pt-[206px]'>
           <Container className='  flex   gap-10 flex-col-reverse md:flex-row items-center'>
 
      
