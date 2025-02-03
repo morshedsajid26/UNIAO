a1db5da63bf922664d986d1f673fc5cf02b9a070
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-02-03 23:20:36 +0600

    blog updated

--- my-project/src/Layer/Blog.jsx
+++ my-project/src/Layer/Blog.jsx
@@ -9,26 +9,26 @@ import { IoMdEye } from 'react-icons/io'
 
 let BlogCard =({src})=> {
   return (
-    <div className='w-[817px] bg-[#1E2127] rounded-[30px] flex items-center gap-[35px] py-[27px] px-[24px]'>
+    <div className='w-[327px] md:w-[817px] bg-[#1E2127] rounded-[30px] flex items-center gap-[35px] py-[14px] md:py-[27px] px-[10px] md:px-[24px]'>
       <div>
-        <img src={src} alt="" />
+        <img className='w-[121px] h-[107px] md:w-full md:h-full' src={src} alt="" />
       </div>
 
-      <div className='w-[330px]'>
-              <p className='font-OS text-[30px] text-white'>Game Keys The Best 
+      <div className='w-[160px] md:w-[330px]'>
+              <p className='font-OS text-[14px] md:text-[30px] text-white'>Game Keys The Best 
               Websites for Affordable 
               Gaming Deals</p>
 
-              <p className='font-OS text-[28px] text-[#E614FC] mt-9 mb-[21px]'>19 Sep, 23</p>
+              <p className='font-OS text-[13px] md:text-[28px] text-[#E614FC] md:mt-9 md:mb-[21px] my-[9px] '>19 Sep, 23</p>
 
               <div className="icon flex gap-[22px] text-white/40">
              
               <div className='flex items-center gap-[7px] '>
-              <PiClockCountdownFill className='w-[31px] h-[31px] ' />
-              <p className='font-OS text-[20px]'>3 min read</p>
+              <PiClockCountdownFill className='w-[17px] h-[17px] md:w-[31px] md:h-[31px] ' />
+              <p className='font-OS text-[13px] md:text-[20px]'>3 min read</p>
               </div>
 
-              <div className='flex items-center gap-[7px]  '>
+              <div className='md:flex items-center gap-[7px] hidden  '>
               <IoMdEye className='w-[33px] h-[26px]'/>
               <p className='font-OS text-[20px]'>1230</p>
               </div>
@@ -65,7 +65,7 @@ const Blog = () => {
             </div>
 
 
-            <div className='flex justify-between mt-[42px]'>
+            <div className='flex flex-col md:flex-row md:justify-between mt-[42px] gap-[17px] md:gap-0'>
               <BlogCard
               src={blog1}/>
             
