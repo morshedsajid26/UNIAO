9873d2fecb41e042cf6112259bb736f705a8dde7
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-02-05 23:38:23 +0600

    li active running

--- my-project/src/Layer/Li.jsx
+++ my-project/src/Layer/Li.jsx
@@ -4,7 +4,18 @@ import { NavLink } from 'react-router-dom'
 const Li = ({className,src,imageClass,href}) => {
   return (
     <li className='group' >
-        <NavLink className={`w-[58px] h-[58px] md:w-20 md:h-20 flex justify-center hover: items-center relative after:absolute after:content-[""] after:w-full after:h-full after:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% after:top-0 after:left-0 z-10 after:-z-10 after:opacity-0 hover:after:opacity-100 after:duration-300  after:cursor-pointer !transition-all after:border   after:rounded-[10px] border md:border-0 rounded-[10px]  ${className} `} to={href} >
+        <NavLink className={`w-[58px] h-[58px] md:w-20 md:h-20 flex justify-center hover: items-center relative after:absolute after:content-[""] after:w-full after:h-full after:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% after:top-0 after:left-0 z-10 after:-z-10 after:opacity-0 hover:after:opacity-100 after:duration-300  after:cursor-pointer !transition-all after:border   after:rounded-[10px] border md:border-0 rounded-[10px]  ${className} `} to={href}
+        
+        // end
+        // style={({ isActive }) => ({
+        //   background: isActive ? "#262626" : "#767676",
+          
+          
+          
+          
+          
+        // })} 
+        >
             <img className={imageClass} src={src} alt="" />
         </NavLink>
     </li>
