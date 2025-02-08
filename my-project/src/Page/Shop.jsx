d599d103ba967d1517f05b0afb98f98925723c02
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-02-08 23:41:58 +0600

    shop responsive running

--- my-project/src/Page/Shop.jsx
+++ my-project/src/Page/Shop.jsx
@@ -6,6 +6,7 @@ import Switch from '../Component/Switch'
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md'
 import CheckBox from '../Component/CheckBox'
 import { HiLightningBolt } from 'react-icons/hi'
+import { FaFilter } from 'react-icons/fa'
 
 const Shop = () => {
   let [delivery,setDelivery] = useState(false);
@@ -15,14 +16,31 @@ const Shop = () => {
   let [show,setShow]= useState(false);
   return (
     <div>
-        <Container className='mt-[19px] flex flex-row md:gap-6 gap-2'>
+       <Container>
+       <div className='mt-[19px] flex flex-row md:gap-6 gap-2'>
             <button className='md:py-[11px] py-[5px] px-[6.5px] md:px-[14px] border rounded-md md:rounded-xl hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% hover:border-transparent transition-all duration-500 text-white font-OS text-[14px] md:text-[24px] '>PlayStation Network</button>
             <button className='md:py-[11px] py-[5px] px-[6.5px] md:px-[14px] border rounded-md md:rounded-xl hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% hover:border-transparent transition-all duration-500 text-white font-OS text-[14px] md:text-[24px] '>PS5</button>
             <button className='md:py-[11px] py-[5px] px-[6.5px] md:px-[14px] border rounded-md md:rounded-xl hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% hover:border-transparent transition-all duration-500 text-white font-OS text-[14px] md:text-[24px] '>PS4</button>
             <button className='md:py-[11px] py-[5px] px-[6.5px] md:px-[14px] border rounded-md md:rounded-xl hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% hover:border-transparent transition-all duration-500 text-white font-OS text-[14px] md:text-[24px] '>PSVR</button>
-        </Container>
+        </div>
 
-        <Container className='flex gap-[29.5px] mt-20'>
+
+        <div className='flex items-center justify-between mt-[51px] md:hidden '>
+                <h1 className='text-[16px] md:text-[36px] font-OS text-white'>Weekly Deals</h1>
+                <div className='h-[1px] md:h-[2px] w-[66px] md:w-[1140px] bg-custom-gradient'
+                ></div>
+
+
+
+                
+
+                <button className='flex items-center gap-[3px] border py-[7px] md:py-3 px-[10.5px] md:px-[18px] text-white font-OS text-[12px] md:text-[24px] rounded-[10px]'>
+                <FaFilter className='w-[13px] h-3' />Filter By
+                    </button>
+            </div>
+       </Container>
+
+        <Container className='flex gap-[29.5px] mt-3 md:mt-20'>
           <div className="left w-[393px]  hidden md:flex md:flex-col gap-10" >
 
 
@@ -392,7 +410,7 @@ const Shop = () => {
           </div>
          
          
-          <div className="right w-[327px] md:w-[1240px]   ">
+          <div className="right w-[327px] md:w-[1240px]">
             <Paginate itemsPerPage={15}/>
           </div>
         </Container>
