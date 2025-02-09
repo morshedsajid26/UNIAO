e18eff460b304e91fc5778d217103bde1eceeddb
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-02-09 23:03:58 +0600

    pruduct details running

--- my-project/src/Page/ProductDeatils.jsx
+++ my-project/src/Page/ProductDeatils.jsx
@@ -3,6 +3,7 @@ import Container from '../Layer/Container'
 
 import productDetails from '/productDetails.png'
 import { HiMiniInformationCircle } from 'react-icons/hi2'
+import { BiSolidError } from 'react-icons/bi'
 
 const ProductDeatils = () => {
   return (
@@ -22,10 +23,19 @@ const ProductDeatils = () => {
       </div>
 
       <div className="right w-[817px] bg-gradient-to-b from-[#18C5E1] from-0% via-[#8666EF] via-30% to-[#FB01FF] to-100% p-[2px] rounded-[25px] ">
+      <div className='bg-[#1E2127] h-full rounded-[25px] py-7 px-[59px]'>
 
-        <div className='bg-[#1E2127] h-full rounded-[25px] py-7 px-[59px]'>d</div>
+
+        <div className="top w-full flex items-center justify-center pb-5 border-b border-white/20">
+        <BiSolidError className='w-[30px] h-7 text-white' />
+        <p className='font-OS text-white text-[23px] ml-2.5'>Can not be activated in Sweden</p>
+
+        </div>
 
 
+
+
+        </div>
       </div>
 
 
