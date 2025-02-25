a6604c0358fe160df8bedf70000e1997bb925432
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-02-25 23:09:39 +0600

    game media running

--- my-project/src/Layer/GameMedia.jsx
+++ my-project/src/Layer/GameMedia.jsx
@@ -1,30 +1,41 @@
 import React from 'react'
 import Container from './Container'
 
+import GameMedia1 from '/GameMedia1.png'
+import GameMedia2 from '/GameMedia2.png'
+import GameMedia3 from '/GameMedia3.png'
+import GameMedia4 from '/GameMedia4.png'
+
 const GameMedia = () => {
   return (
     <div>
-        <Container>
+        <Container className='pb-[192px]'>
         <div className='flex items-center  justify-between mb-[35px]'>
                 <h1 className='text-[14px] md:text-[36px] font-OS text-white'>Game Media</h1>
-                <div className='h-[1px] md:h-[2px] w-[40px] md:w-[1112px] bg-custom-gradient'
+                <div className='h-[1px] md:h-[2px] w-[40px] md:w-[1312px] bg-custom-gradient'
                 ></div>
 
 
 
                 
 
-                <button className='border py-[7px] md:py-3 px-[10.5px] md:px-[18px] text-white font-OS text-[12px] md:text-[24px] rounded-[10px]'>
-                        Browse All
-                    </button>
+                
             </div>
 
 
-        <div className='w-full grid grid-rows-1 grid-cols-4 justify-between'>
-            <div className="child w-[394px] ">1</div>
-            <div className="child w-[394px] ">2</div>
-            <div className="child w-[394px] ">3</div>
-         <div className="child w-[394px] ">4</div>
+        <div className='w-full grid grid-rows-1 grid-cols-4 gap-[29px]'>
+            <div className="child w-[394px] ">
+              <img src={GameMedia1} alt="" />
+            </div>
+            <div className="child w-[394px] ">
+            <img src={GameMedia2} alt="" />
+            </div>
+            <div className="child w-[394px] ">
+            <img src={GameMedia3} alt="" />
+            </div>
+         <div className="child w-[394px] ">
+         <img src={GameMedia4} alt="" />
+         </div>
 
 
         </div>
