45b4450cbc78d056f40befcb05968e085e12f6e4
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-03-10 23:33:31 +0600

    checkout running

--- my-project/src/Page/CheckOut.jsx
+++ my-project/src/Page/CheckOut.jsx
@@ -5,7 +5,19 @@ const CheckOut = () => {
   return (
     <div>
         <Container>
-            
+        <div className='flex items-center justify-between mb-[35px]'>
+                <h1 className='text-[14px] md:text-[36px] font-OS text-white'>Checkout</h1>
+                <div className='h-[1px] md:h-[2px] w-[40px] md:w-[1112px] bg-custom-gradient'
+                ></div>
+
+
+
+                
+
+                <button className='border py-[7px] md:py-3 px-[10.5px] md:px-[18px] text-white font-OS text-[12px] md:text-[24px] rounded-[10px]'>
+                Continue Shopping
+                    </button>
+            </div>
         </Container>
     </div>
   )
