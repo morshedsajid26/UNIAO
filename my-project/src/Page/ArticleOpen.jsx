f2e4b5981facf627d55507d6af01e8b9b7201dd6
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-03-16 23:08:44 +0600

    article page added

---
+++ my-project/src/Page/ArticleOpen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from '../Layer/Container'
+
+const ArticleOpen = () => {
+  return (
+    <div><Container>
+        article
+        </Container></div>
+  )
+}
+
+export default ArticleOpen
