e4202e633fcea923f97926c3bcf67ed181ad48d4
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-03-16 23:07:37 +0600

    blog page added

---
+++ my-project/src/Page/Blog.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '../Layer/Container'
+
+const Blog = () => {
+  return (
+    <div>
+        <Container>
+            
+        </Container>
+    </div>
+  )
+}
+
+export default Blog
