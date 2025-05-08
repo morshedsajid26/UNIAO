ea3af25cbdc70e7f6d3ec98d1101833872053d81
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-05-08 23:38:52 +0600

    checkout payment icon done

--- my-project/src/Page/CheckOut.jsx
+++ my-project/src/Page/CheckOut.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 import Container from '../Layer/Container'
+import payment1 from '/payment1.png'
+import payment2 from '/payment2.png'
+import payment3 from '/payment3.png'
+import payment4 from '/payment4.png'
+import payment5 from '/payment5.png'
+import payment6 from '/payment6.png'
+import payment7 from '/payment7.png'
+import payment8 from '/payment8.png'
+import payment9 from '/payment9.png'
+import payment10 from '/payment10.png'
+import payment11 from '/payment11.png'
+
+let Icon= ({src})=>{
+  return(
+    <img className='w-[79.05px] h-[53.85px]' src={src} alt="" />
+
+  )
+}
 
 const CheckOut = () => {
+
   return (
     <div>
         <Container>
@@ -17,6 +36,28 @@ const CheckOut = () => {
                 Continue Shopping
                     </button>
             </div>
+
+        
+        <div>
+          <div className="left w-[1095px]">
+            <div className="icon grid grid-cols-11">
+              <Icon src={payment1}/>
+              <Icon src={payment2}/>
+              <Icon src={payment3}/>
+              <Icon src={payment4}/>
+              <Icon src={payment5}/>
+              <Icon src={payment6}/>
+              <Icon src={payment7}/>
+              <Icon src={payment8}/>
+              <Icon src={payment9}/>
+              <Icon src={payment10}/>
+              <Icon src={payment11}/>
+
+            </div>
+
+          </div>
+          <div className="right w-[526px]"></div>
+        </div>
 
 
            
