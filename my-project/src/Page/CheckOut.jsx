46f748c2384d850fcb6e3213d1195bbbb037885c
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-05-10 23:36:54 +0600

    checkout responsive running

--- my-project/src/Page/CheckOut.jsx
+++ my-project/src/Page/CheckOut.jsx
@@ -54,7 +54,7 @@ const CheckOut = () => {
             </div>
 
         
-        <div className='flex flex-col md:flex-row justify-between'>
+        <div className='flex flex-col md:flex-row justify-between '>
           <div className="left w-[1095px]">
             <div className="icon grid grid-rows-2 grid-cols-6  md:grid-cols-11 gap-y-2.5 md:gap-0">
               <Icon src={payment5}/>
@@ -89,28 +89,28 @@ const CheckOut = () => {
             </div>
 
           </div>
-          <div className=" right w-[526px]">
+          <div className=" right w-[526px] mt-[63px] md:mt-0">
             <div className="up"></div>
-            <div className="down border border-white/15 rounded-[15px] px-8 pt-8 pb-[48px] mb-[199px] ">
-              <div className='border-b w-[462px] border-white/15 flex flex-col items-center pb-[35px]'>
+            <div className="down w-[327px] md:w-full border border-white/15 rounded-[15px] px-5 md:px-8 pt-8 pb-[48px] mb-[199px] ">
+              <div className='border-b w-[288px] md:w-[462px] border-white/15 flex flex-col items-center pb-[14px] md:pb-[35px]'>
               <div className="logo flex items-end gap-[6px] ">
-                <img className='w-7 h-[27px] md:w-[41.84px] md:h-[40.13px]' src={star} alt="" />
-                <img className='w-[86.86px] h-[18.94px] md:w-[144.53px] md:h-[31.52px]' src={trustpilot} alt="" />
+                <img className='w-[26px] h-[24px] md:w-[41.84px] md:h-[40.13px]' src={star} alt="" />
+                <img className='w-[89.86px] h-[19.5px] md:w-[144.53px] md:h-[31.52px]' src={trustpilot} alt="" />
               </div>
               
               
               
-              <p className='font-Inter text-[10px] md:text-[20px] text-[#787A7D]'>TrustScore 5.0 | 457 Reviews</p>
+              <p className='font-Inter text-[11px] md:text-[20px] text-[#787A7D]'>TrustScore 5.0 | 457 Reviews</p>
               </div>
 
               <div className='flex flex-col items-center '>
-              <div className='flex gap-[5px] md:gap-[9.5px] md:mt-[40.4px] md:mb-[4.85px]'>
-    <img className='w-[15.8px] h-[18.5px] md:w-[32.45px] md:h-[38.15px] ' src={secure} alt="" />
+              <div className='flex gap-[5px] md:gap-[9.5px] mt-[14px] md:mt-[40.4px] md:mb-[4.85px]'>
+    <img className='w-[20.3px] h-[23.75px] md:w-[32.45px] md:h-[38.15px] ' src={secure} alt="" />
 
-    <p className='font-pop text-[13px] md:text-[28px] text-white'>Safe & Secure</p>
+    <p className='font-pop text-[17px] md:text-[28px] text-white'>Safe & Secure</p>
    </div>
 
-   <p className='font-Inter text-[10px] md:text-[20px] text-[#787A7D] '>100% Secure, 24/7 Support</p>
+   <p className='font-Inter text-[11px] md:text-[20px] text-[#787A7D] '>100% Secure, 24/7 Support</p>
               </div>
             </div>
           </div>
