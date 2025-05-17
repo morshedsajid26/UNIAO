bfe4716b74b170c7ab6979f1ef71a9a14984dfed
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-05-17 21:57:52 +0600

    rootlayout fixed

--- my-project/src/Component/Header.jsx
+++ my-project/src/Component/Header.jsx
@@ -5,7 +5,7 @@ import Navbar from './Navbar'
 const Header = () => {
   return (
     <div>
-        <SearchBar/>
+        
         
     </div>
   )
--- my-project/src/Layer/RootLayout.jsx
+++ my-project/src/Layer/RootLayout.jsx
@@ -4,11 +4,12 @@ import React from 'react'
 import Footer from '../Component/Footer'
 import { Outlet } from 'react-router-dom'
 import Header from '../Component/Header'
+import SearchBar from '../Component/SearchBar'
 
 const RootLayout = () => {
   return (
     <div className='bg-[#15181D] overflow-hidden'>
-        <Header/>
+        <SearchBar/>
         <Outlet/>
         <Footer/>
     </div>
