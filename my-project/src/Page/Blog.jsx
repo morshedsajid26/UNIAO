a343d359d64986e724171dd5537ac484439adead
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-05-22 22:00:32 +0600

    blog responsive running

--- my-project/src/Page/Blog.jsx
+++ my-project/src/Page/Blog.jsx
@@ -20,24 +20,35 @@ const Blog = () => {
    <div className=''>
         <Container>
            
-           <div className='flex items-center  pt-[51px] pb-[42px] '>
-                      <h3 className='text-white font-Inter text-4xl mr-[34px]'>Blog Articles</h3>
-
-                      <div className='w-[756px] flex justify-between'>
-                        <span className='py-3 px-[22px] border rounded-[10px] text-white font-OS text-[24px] hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% cursor-pointer transition-all duration-300 '>All</span>
-                        <span className='py-3 px-[22px] border rounded-[10px] text-white font-OS text-[24px] hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% cursor-pointer transition-all duration-300'>Gaming</span>
-                        <span className='py-3 px-[22px] border rounded-[10px] text-white font-OS text-[24px] hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% cursor-pointer transition-all duration-300'>Investment</span>
-                        <span className='py-3 px-[22px] border rounded-[10px] text-white font-OS text-[24px] hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% cursor-pointer transition-all duration-300'>Software</span>
-                        <span className='py-3 px-[22px] border rounded-[10px] text-white font-OS text-[24px] hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% cursor-pointer transition-all duration-300'>News</span>
+           <div>
+                      <div className='flex items-center   pt-4 md:pt-[51px] pb-2.5 md:pb-[42px] '>
+                                 <h3 className='text-white font-Inter text-[16px] md:text-[36px] mr-[34px]'>Blog Articles</h3>
+           
+                                 <div className='w-[756px]  justify-between hidden md:flex'>
+                                   <span className='py-[7px] md:py-3 px-2 md:px-[22px] border rounded-[10px] text-white font-OS text-[12px] md:text-[24px] hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% cursor-pointer transition-all duration-300 '>All</span>
+                                   <span className='py-[7px] md:py-3 px-2 md:px-[22px] border rounded-[10px] text-white font-OS text-[12px] md:text-[24px] hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% cursor-pointer transition-all duration-300'>Gaming</span>
+                                   <span className='py-[7px] md:py-3 px-2 md:px-[22px] border rounded-[10px] text-white font-OS text-[12px] md:text-[24px] hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% cursor-pointer transition-all duration-300'>Investment</span>
+                                   <span className='py-[7px] md:py-3 px-2 md:px-[22px] border rounded-[10px] text-white font-OS text-[12px] md:text-[24px] hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% cursor-pointer transition-all duration-300'>Software</span>
+                                   <span className='py-[7px] md:py-3 px-2 md:px-[22px] border rounded-[10px] text-white font-OS text-[12px] md:text-[24px] hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% cursor-pointer transition-all duration-300'>News</span>
+                                 </div>
+           
+                                 <div className="searchBar w-[186.85px] md:w-[350px] relative  md:ml-[320px] ">
+                                                 <input className='bg-[#1E2127] text-[#B2B8C2] font-OS text-[12px] md:text-[22px] outline-none w-full py-[7.5px] md:py-3 px-[12.5px] md:px-[23px] rounded-[10px]' type="text" placeholder='Search blog article...' />
+                                                
+                                                 <IoSearch className='w-[22.5px] h-[22.5px] text-[#B2B8C2] absolute top-1/2 -translate-y-1/2 right-4' />
+                                                 
+                                 </div>
+           
+                                 
+                         </div>
+                         <div className='w-[327px]  justify-between md:hidden flex pb-7 '>
+                                   <span className='py-[7px] md:py-3 px-2 md:px-[22px] border rounded-[10px] text-white font-OS text-[12px] md:text-[24px] hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% cursor-pointer transition-all duration-300 '>All</span>
+                                   <span className='py-[7px] md:py-3 px-2 md:px-[22px] border rounded-[10px] text-white font-OS text-[12px] md:text-[24px] hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% cursor-pointer transition-all duration-300'>Gaming</span>
+                                   <span className='py-[7px] md:py-3 px-2 md:px-[22px] border rounded-[10px] text-white font-OS text-[12px] md:text-[24px] hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% cursor-pointer transition-all duration-300'>Investment</span>
+                                   <span className='py-[7px] md:py-3 px-2 md:px-[22px] border rounded-[10px] text-white font-OS text-[12px] md:text-[24px] hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% cursor-pointer transition-all duration-300'>Software</span>
+                                   <span className='py-[7px] md:py-3 px-2 md:px-[22px] border rounded-[10px] text-white font-OS text-[12px] md:text-[24px] hover:bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% cursor-pointer transition-all duration-300'>News</span>
+                                 </div>
                       </div>
-
-                      <div className="searchBar w-[350px] relative  ml-[320px]">
-                                      <input className='bg-[#1E2127] text-[#B2B8C2] font-OS text-[22px] outline-none w-full py-3 px-[23px] rounded-[10px]' type="text" placeholder='Search blog article...' />
-                                     
-                                      <IoSearch className='w-[22.5px] h-[22.5px] text-[#B2B8C2] absolute top-1/2 -translate-y-1/2 right-4' />
-                                      
-                                  </div>
-              </div>
 
               <div className=' bg-[url("/public/BlogBanner.png")] bg-cover bg-center bg-no-repeat w-[1664px] relative after:absolute after:w-full after:h-full after:bg-black/20 after:top-0 after:left-0 after:rounded-[25px] after:-z-30 z-30 rounded-[25px]  '>
 
