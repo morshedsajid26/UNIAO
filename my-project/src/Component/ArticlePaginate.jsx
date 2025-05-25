9419b35650bd91c0cc72c92a6a0ccedfaa8af8bf
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-05-25 22:18:16 +0600

    blog responsive running

--- my-project/src/Component/ArticlePaginate.jsx
+++ my-project/src/Component/ArticlePaginate.jsx
@@ -7,27 +7,29 @@ import { MdKeyboardArrowRight, MdOutlineArrowBackIos, MdOutlineArrowForwardIos }
 import ArticleImage1 from '/ArticleImage1.png'
 import { IoTimerSharp } from 'react-icons/io5';
 
-const items = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 13, 14,0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,1, 2, 3, 4, 5, 6, 7];
+const items = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 13, 14,0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,];
 
 let ArticleCard = ({src})=>{
   return (
 
-  <div className='w-[526px] bg-[#1E2127] px-6 pt-[27px] pb-[42px] rounded-[25px]'>
-    <img  src={src} alt="" />
+  <div className='w-[327px] md:w-[526px] bg-[#1E2127] py-[14px] pl-2.5 md:px-6 md:pt-[27px] md:pb-[42px] rounded-[25px] flex md:inline-block gap-[14px] md:gap-0'>
+    <img className='w-[121px] md:w-auto h-[107px] md:h-auto'  src={src} alt="" />
 
-    <p className='font-Inter text-white text-[30px] w-[323px] mt-[25px] mb-[36px]'>Game Keys The Best 
+   <div className=''>
+     <p className='font-Inter text-white text-[14px] md:text-[30px] w-[160px] md:w-[323px] md:mt-[25px] md:mb-[36px]'>Game Keys The Best 
     Websites for Affordable 
     Gaming Deals</p>
 
 
-    <p className='text-[28px] font-Inter text-[#E614FC]'>19 Sep, 23</p>
+    <p className='text-[13px] md:text-[28px] font-Inter text-[#E614FC]'>19 Sep, 23</p>
 
-    <p className='text-white font-OS text-[20px] flex items-center gap-[7px] mt-[31px]'>
-    <IoTimerSharp className='text-white/40 h-[31px] w-[31px]' />
+    <p className='text-white font-OS text-[14px] md:text-[20px] flex items-center gap-[7px] md:mt-[31px]'>
+    <IoTimerSharp className='text-white/40 h-[17px] md:h-[31px] w-[17px] md:w-[31px]' />
 
     3 min read
 
     </p>
+   </div>
 
   </div>
 
@@ -74,7 +76,7 @@ const ArticlePaginate = ({itemsPerPage}) => {
 
   return (
     <>
-      <div className='flex flex-col md:flex-row md:gap-x-[29.5px] flex-wrap md:gap-y-[54px]'>
+      <div className='flex flex-col md:flex-row gap-5 md:gap-x-[29.5px] flex-wrap md:gap-y-[54px]'>
       <Items currentItems={currentItems} />
       </div>
       <ReactPaginate
@@ -92,11 +94,11 @@ const ArticlePaginate = ({itemsPerPage}) => {
        
         pageClassName="page-item liCss flex items-center justify-center text-[13px] md:text-[24px] text-white ml-[13px] md:ml-6  "
 
-         pageLinkClassName="page-link aCss w-[33px] md:w-[59px] h-[33px]  md:h-[59px] rounded-full border flex items-center justify-center "
+         pageLinkClassName="page-link aCss w-[33px] md:w-[59px] h-[33px]  md:h-[59px] rounded-full border flex items-center justify-center"
 
          activeClassName="active bg-gradient-to-bl from-[#FC00FF] from-1% via-[#7E6EEF] via-50% to-[#00DBDE] to-100% rounded-full"
         
-         containerClassName="pagination flex  items-center justify-center pt-[126px] md:mt-0"
+         containerClassName="pagination flex  items-center justify-center pt-6 md:pt-[126px] gap-0 md:mt-0"
       />
     </>
   );
